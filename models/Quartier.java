516aab33e0ee7e7727be1bd44bc1d49f7feebfd3
BatLeDev <baptiste.guerny@gmail.com>
2023-05-23 11:06:02 +0200

    chg name

--- models/Quartier.java
+++ models/Quartier.java
@@ -62,7 +62,7 @@ public class Quartier {
         }
 
         this.idQuartier = id;
-        this.setNomQuartier(nom);
+        this.setNom(nom);
         this.setLgPisteCyclable(lgPisteCyclable);
 
         // add the quartier to the quartierList
@@ -76,7 +76,7 @@ public class Quartier {
      * 
      * @param nomQuartier a String representing the name of the quartier (not null or empty)
      */
-    public void setNomQuartier(String nomQuartier) {
+    public void setNom(String nomQuartier) {
         if (nomQuartier == null || nomQuartier.isEmpty()) {
             throw new IllegalArgumentException("models.Quartier.setNom : Le parametre nom n'est pas valide");
         }
