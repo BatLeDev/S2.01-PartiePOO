8b0db69abc0162a77c4921264b433a10eefdf4bb
Matéo <nouvion.e2201090@etud.univ-ubs.fr>
2023-05-31 20:13:15 +0200

    TestJour fini

--- TestJour.java
+++ TestJour.java
@@ -35,79 +35,144 @@ public class TestJour {
         } catch (IllegalArgumentException e) {
             printError("ECHEC : Constructor test failed: " + e.getMessage());
         }
-    }
-
-    private static void testDeleteJour() {
-        System.out.println("\nTest de deleteJour");
-        new Jour("2023-06-15", "Jeudi", null, 21.5);
-        Jour.deleteJour("2023-06-15");
-
-        if (Jour.getJour("2023-06-15") == null) {
-            printOk("OK : deleteJour test passed");
-        } else {
-            printError("ECHEC : deleteJour test failed");
+    
+        // Invalid date format
+        try {
+            new Jour("2023/05/30", "Lundi", null, 25.5);
+            printError("ECHEC : Constructor test failed: Invalid date format");
+        } catch (IllegalArgumentException e) {
+            printOk("OK : Constructor test passed: Invalid date format");
+        }
+    
+        // Existing date
+        try {
+            new Jour("2023-05-30", "Mardi", null, 24.8);
+            new Jour("2023-05-30", "Mercredi", null, 23.7);
+            printError("ECHEC : Constructor test failed: Existing date");
+        } catch (IllegalArgumentException e) {
+            printOk("OK : Constructor test passed: Existing date");
         }
     }
 
+    
+    private static void testDeleteJour() {
+        System.out.println("\nTest de deleteJour");
+        // Valid case
+        try {
+            new Jour("2023-05-31", "Lundi", null, 25.5);
+            Jour.deleteJour("2023-05-31");
+            Jour deletedJour = Jour.getJour("2023-05-31");
+            if (deletedJour == null) {
+                printOk("OK : deleteJour test passed");
+            } else {
+                printError("ECHEC : deleteJour test failed: Jour still exists");
+            }
+        } catch (IllegalArgumentException e) {
+            printError("ECHEC : deleteJour test failed: " + e.getMessage());
+        }
+    }
+
+
     private static void testSetJourString() {
         System.out.println("\nTest de setJour(String)");
-        Jour jour3 = new Jour("2023-04-25", "Lundi", null, 25.5);
-        jour3.setJour("Mardi");
+        Jour jour = new Jour("2023-06-01", "Lundi", null, 25.5);
 
-        if (jour3.getJour().equals("Mardi")) {
-            printOk("OK: setJour(String) test passed");
-        } else {
-            printError("ECHEC : setJour(String) test failed");
+        // Valid case
+        try {
+            jour.setJour("Mardi");
+            if (jour.getJour().equals("Mardi")) {
+                printOk("setJour(String) test passed");
+            } else {
+                printError("setJour(String) test failed: Incorrect day");
+            }
+        } catch (IllegalArgumentException e) {
+            printError("setJour(String) test failed: " + e.getMessage());
+        }
+
+        // Invalid day
+        try {
+            jour.setJour("Sundae");
+            printError("setJour(String) test failed: Invalid day");
+        } catch (IllegalArgumentException e) {
+            printOk("setJour(String) test passed: Invalid day");
         }
     }
 
     private static void testSetJourInt() {
         System.out.println("\nTest de setJour(int)");
-        Jour jour4 = new Jour("2023-05-14", "Lundi", null, 25.5);
-        jour4.setJour(2);
+        Jour jour = new Jour("2023-06-02", "Lundi", null, 25.5);
 
-        if (jour4.getJour().equals("Mercredi")) {
-            printOk("OK : setJour(int) test passed");
-        } else {
-            printError("ECHEC : setJour(int) test failed");
+        // Valid case
+        try {
+            jour.setJour(2);
+            if (jour.getJour().equals("Mercredi")) {
+                printOk("setJour(int) test passed");
+            } else {
+                printError("setJour(int) test failed: Incorrect day");
+            }
+        } catch (IllegalArgumentException e) {
+            printError("setJour(int) test failed: " + e.getMessage());
+        }
+
+        // Invalid day
+        try {
+            jour.setJour(8);
+            printError("setJour(int) test failed: Invalid day");
+        } catch (IllegalArgumentException e) {
+            printOk("setJour(int) test passed: Invalid day");
         }
     }
 
     private static void testSetTemperatureMoyenne() {
         System.out.println("\nTest de setTemperatureMoyenne");
-        Jour jour5 = new Jour("2023-05-02", "Lundi", null, 25.5);
-        jour5.setTemperatureMoyenne(20.3);
+        Jour jour = new Jour("2023-06-03", "Lundi", null, 25.5);
 
-        if (jour5.getTemperatureMoyenne() == 20.3) {
-            printOk("OK : setTemperatureMoyenne test passed");
-        } else {
-            printError("ECHEC : setTemperatureMoyenne test failed");
+        // Valid case
+        try {
+            jour.setTemperatureMoyenne(28.9);
+            if (jour.getTemperatureMoyenne() == 28.9) {
+                printOk("setTemperatureMoyenne test passed");
+            } else {
+                printError("setTemperatureMoyenne test failed: Incorrect average temperature");
+            }
+        } catch (IllegalArgumentException e) {
+            printError("setTemperatureMoyenne test failed: " + e.getMessage());
+        }
+
+        // Aberrant temperature
+        try {
+            jour.setTemperatureMoyenne(80.0);
+            printError("setTemperatureMoyenne test failed: Aberrant temperature");
+        } catch (IllegalArgumentException e) {
+            printOk("setTemperatureMoyenne test passed: Aberrant temperature");
         }
     }
 
     private static void testEstWeekend() {
         System.out.println("\nTest de estWeekend");
-        Jour jour6 = new Jour("2023-11-01", "Lundi", null, 25.5);
-        Jour jour7 = new Jour("2023-11-02", "Mardi", null, 24.8);
-        Jour jour8 = new Jour("2023-11-03", "Samedi", null, 23.7);
+        Jour jour1 = new Jour("2023-06-04", "Lundi", null, 25.5);
+        Jour jour2 = new Jour("2023-06-05", "Mardi", null, 26.3);
+        Jour jour3 = new Jour("2023-06-06", "Samedi", null, 24.9);
 
-        if (!jour6.estWeekEnd() && !jour7.estWeekEnd() && jour8.estWeekEnd()) {
-            printOk("OK : estWeekend test passed");
+        // Valid case
+        if (jour1.estWeekEnd() == false && jour2.estWeekEnd() == false && jour3.estWeekEnd() == true) {
+            printOk("estWeekend test passed");
         } else {
-            printError("ECHEC : estWeekend test failed");
+            printError("estWeekend test failed: Incorrect weekend detection");
         }
     }
 
     private static void testEstVacances() {
         System.out.println("\nTest de estVacances");
-        Jour jour9 = new Jour("2023-12-01", "Lundi", null, 25.5);
-        Jour jour10 = new Jour("2023-12-02", "Mardi", "Noel", 24.8);
-        Jour jour11 = new Jour("2023-12-03", "Samedi", "Ete", 23.7);
+        Jour jour1 = new Jour("2023-06-07", "Lundi", null, 25.5);
+        Jour jour2 = new Jour("2023-06-08", "Mardi", "Ete", 26.3);
+        Jour jour3 = new Jour("2023-06-09", "Samedi", "Printemps", 24.9);
 
-        if (!jour9.estVacances() && jour10.estVacances() && jour11.estVacances()) {
-            printOk("OK : estVacances test passed");
+        // Valid case
+        if (jour1.estVacances() == false && jour2.estVacances() == true && jour3.estVacances() == true) {
+            printOk("estVacances test passed");
         } else {
-            printError("ECHEC : estVacances test failed");
+            printError("estVacances test failed: Incorrect vacation detection");
         }
     }
 
